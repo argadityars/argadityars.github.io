c77d57ec3e62f5dbd8f1e38bbe3fb5f6d696cc7d
Argaditya RS <dev.argadityars@gmail.com>
2019-08-17 18:48:23 +0700

    add copyright at footer

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,5 @@ import "../scss/main.scss";
 import $ from "jquery";
 import bootstrap from "bootstrap";
 import popper from "popper.js";
+
+document.getElementById("copyright").appendChild(document.createTextNode(new Date().getFullYear()));
