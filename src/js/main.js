e11cba4e089f55455788baec8621c7ebceb92c8c
Argaditya RS <dev.argadityars@gmail.com>
2019-08-17 22:51:46 +0700

    Updates

--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-import "../scss/main.scss";
-import $ from "jquery";
-import bootstrap from "bootstrap";
-import popper from "popper.js";
-
-document.getElementById("copyright").appendChild(document.createTextNode(new Date().getFullYear()));
